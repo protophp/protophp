6ea6bcea942a8df4d7e87831ba8632d07552a859
Arash Ghamkhari <dev@ghamkhari.com>
2019-01-27 10:45:10 +0330

    The ListenerInterface is added

---
+++ src/ListenerInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Proto;
+
+use Evenement\EventEmitterInterface;
+use Proto\Session\SessionManagerInterface;
+use React\EventLoop\LoopInterface;
+
+interface ListenerInterface extends EventEmitterInterface
+{
+    public function __construct($uri, LoopInterface $loop, SessionManagerInterface $sessionManager, array $context = array());
+}
