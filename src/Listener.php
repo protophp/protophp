827e92fb8513a4b57a21ed78454ba40ed3f3141d
Arash Ghamkhari <dev@ghamkhari.com>
2019-01-27 10:45:24 +0330

    The Listener is added

---
+++ src/Listener.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Proto;
+
+use Evenement\EventEmitter;
+use Proto\PromiseTransfer\PromiseTransfer;
+use Proto\PromiseTransfer\PromiseTransferInterface;
+use Proto\Session\SessionInterface;
+use Proto\Session\SessionManagerInterface;
+use React\EventLoop\LoopInterface;
+use React\Socket\ConnectionInterface;
+use React\Socket\Server;
+
+class Listener extends EventEmitter implements ListenerInterface
+{
+    /**
+     * @var SessionManagerInterface
+     */
+    private $sessionManager;
+
+    /**
+     * @var Server
+     */
+    private $server;
+
+    public function __construct($uri, LoopInterface $loop, SessionManagerInterface $sessionManager, array $context = array())
+    {
+        $this->sessionManager = $sessionManager;
+        $this->server = new Server($uri, $loop, $context);
+        $this->server->on('connection', function (ConnectionInterface $conn) {
+
+            $transfer = new PromiseTransfer($conn, $this->sessionManager);
+            $transfer->init();
+
+            $transfer->on('established', function (PromiseTransferInterface $transfer, SessionInterface $session) {
+
+                if (!$session->is('PROTO-CONN')) {
+
+                    // Initial the ProtoConnection
+                    $protoConn = new ProtoConnection();
+
+                    // Emit the connection
+                    $this->emit('connection', [$protoConn]);
+
+                    // Add to the session
+                    $session->set('PROTO-CONN', $protoConn);
+
+                } else
+                    $protoConn = $session->get('PROTO-CONN');
+
+                // Setup the connection in new transfer
+                $protoConn->setup($transfer, $session);
+
+            });
+
+        });
+    }
+
+
+}
