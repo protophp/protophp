ad2c9734f6970aff066f4a562e15b599978504a8
Arash Ghamkhari <dev@ghamkhari.com>
2019-02-13 11:23:10 +0330

    ProtoConnectorInterface updated

--- src/ProtoConnectionInterface.php
+++ src/ProtoConnectionInterface.php
@@ -3,8 +3,15 @@
 namespace Proto;
 
 use Evenement\EventEmitterInterface;
+use React\Promise\Deferred;
+use React\Promise\Promise;
 
 interface ProtoConnectionInterface extends EventEmitterInterface
 {
+    const PROTO_DATA = 0;
+    const PROTO_RPC = 1;
+
     public function send($data, callable $onResponse = null, callable $onDelivery = null);
+
+    public function invoke($call, $params = [], Deferred $deferred = null): Promise;
 }
