f5160ba28f57d33254db1dda71a53703b36a8733
Arash Ghamkhari <dev@ghamkhari.com>
2019-02-13 08:53:15 +0330

    ConnectorInterface updated

--- src/ConnectorInterface.php
+++ src/ConnectorInterface.php
@@ -5,10 +5,15 @@ namespace Proto;
 use Evenement\EventEmitterInterface;
 use Proto\Session\SessionManagerInterface;
 use React\EventLoop\LoopInterface;
+use React\Promise\Promise;
 
 interface ConnectorInterface extends EventEmitterInterface
 {
     public function __construct(string $uri, LoopInterface $loop, SessionManagerInterface $sessionManager, string $sessionKey = null, array $options = array());
+
+    public function send($data, callable $onResponse = null, callable $onDelivery = null);
+
+    public function invoke($call, $params = []): Promise;
 
     public function connect();
 }
