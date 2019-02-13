d284fe6bcf02920a8589283013123e623cf18f98
Arash Ghamkhari <dev@ghamkhari.com>
2019-02-13 17:47:55 +0330

    The ProtoException is added

---
+++ src/ProtoException.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Proto;
+
+class ProtoException implements \Throwable
+{
+    private $name;
+    private $message;
+    private $code;
+
+    public function __construct(string $name = "", string $message = "", int $code = 0)
+    {
+        $this->name = $name;
+        $this->message = $message;
+        $this->code = $code;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getMessage()
+    {
+
+    }
+
+    public function getCode()
+    {
+
+    }
+
+    public function getFile()
+    {
+    }
+
+    public function getLine()
+    {
+    }
+
+    public function getTrace()
+    {
+    }
+
+    public function getTraceAsString()
+    {
+    }
+
+    public function getPrevious()
+    {
+    }
+
+    public function __toString()
+    {
+        return "[$this->name] [$this->code] $this->message";
+    }
+}
