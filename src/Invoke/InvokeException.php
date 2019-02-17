331d51a392d424209da0e99546c5a5be9fc144f5
Arash Ghamkhari <dev@ghamkhari.com>
2019-02-17 23:27:23 +0330

    The exception messages added

--- src/Invoke/InvokeException.php
+++ src/Invoke/InvokeException.php
@@ -2,6 +2,8 @@
 
 namespace Proto\Invoke;
 
+use Throwable;
+
 class InvokeException extends \Exception
 {
     const ERR_INVALID_INVOKE = 100;
@@ -10,4 +12,18 @@ class InvokeException extends \Exception
     const ERR_METHOD_NOT_FOUND = 111;
     const ERR_OPERATION_NOT_PERMITTED = 120;
     const ERR_UNKNOWN = 130;
+
+    const MSG = [
+        100 => 'Invalid invoke!',
+        101 => 'Invalid params!',
+        110 => 'Class not found!',
+        111 => 'Method not found!',
+        120 => 'Operation not permitted!',
+        130 => 'Unknown error!',
+    ];
+
+    public function __construct(string $message = "", int $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message != '' ? $message : self::MSG[$code], $code, $previous);
+    }
 }
